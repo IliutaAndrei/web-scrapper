6f2326530308277b0ab62070775057addfa478b8
IliutaAndrei <andreiiliuta586@yahoo.com>
2026-05-27 10:48:27 +0300

    Delete protected pages from browser cache when click Back

--- app.py
+++ app.py
@@ -16,6 +16,15 @@ app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.config["SECRET_KEY"] = SESSION_SECRET_KEY
 
+@app.after_request
+def add_no_cache_headers(response):
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+
+    return response
+
+
 @app.before_request
 def require_login():
     allowed_routes = ["login", "static"]
